39aeee3eea816e527eb7b81ab09e61e066e7e774
22302189 <liam.debast78@laposte.net>
2026-02-16 12:29:36 +0100

    ajout de master et worker

--- distributedMC_step1/MasterSocket.java
+++ distributedMC_step1/MasterSocket.java
@@ -1,16 +1,19 @@
+package distributedMC_step1;
+
 import java.io.*;
 import java.net.*;
 /** Master is a client. It makes requests to numWorkers.
- *   
+ *
  */
 public class MasterSocket {
-    static int maxServer = 8;
-    static final int[] tab_port = {25545,25546,25547,25548,25549,25550,25551,25552};
+    static int maxServer = 12;
+    static final int[] tab_port = {25545,25546,25547,25548,25549,25550,25551,25552,25553,25554,25555,25556};
     static String[] tab_total_workers = new String[maxServer];
     static final String ip = "127.0.0.1";
     static BufferedReader[] reader = new BufferedReader[maxServer];
     static PrintWriter[] writer = new PrintWriter[maxServer];
     static Socket[] sockets = new Socket[maxServer];
+
 
     public static void main(String[] args) throws Exception {
 
@@ -34,7 +37,7 @@ public class MasterSocket {
             System.out.println(numWorkers);
         }
         catch(IOException ioE){
-           ioE.printStackTrace();
+            ioE.printStackTrace();
         }
 
         for (int i=0; i<numWorkers; i++){
@@ -43,72 +46,74 @@ public class MasterSocket {
                 s = bufferRead.readLine();
                 System.out.println("You select " + s);
             }
-                catch(IOException ioE){
+            catch(IOException ioE){
                 ioE.printStackTrace();
             }
         }
-           //create worker's socket
-           for(int i = 0 ; i < numWorkers ; i++) {
-               sockets[i] = new Socket(ip, tab_port[i]);
-               System.out.println("SOCKET = " + sockets[i]);
 
-               reader[i] = new BufferedReader( new InputStreamReader(sockets[i].getInputStream()));
-               writer[i] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(sockets[i].getOutputStream())),true);
-           }
+        //create worker's socket
+        for(int i = 0 ; i < numWorkers ; i++) {
+            sockets[i] = new Socket(ip, tab_port[i]);
+            System.out.println("SOCKET = " + sockets[i]);
 
-           String message_to_send;
-           message_to_send = String.valueOf(totalCount);
+            reader[i] = new BufferedReader( new InputStreamReader(sockets[i].getInputStream()));
+            writer[i] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(sockets[i].getOutputStream())),true);
+        }
 
-           String message_repeat = "y";
-           long stopTime, startTime;
+        String message_to_send;
+        message_to_send = String.valueOf(totalCount);
 
-           while (message_repeat.equals("y")){
+        String message_repeat = "y";
 
-               startTime = System.currentTimeMillis();
-               // initialize workers
-               for(int i = 0 ; i < numWorkers ; i++) {
-                   writer[i].println(message_to_send);          // send a message to each worker
-               }
+        long stopTime, startTime;
 
-               //listen to workers's message
-               for(int i = 0 ; i < numWorkers ; i++) {
-                   tab_total_workers[i] = reader[i].readLine();      // read message from server
-                   System.out.println("Client sent: " + tab_total_workers[i]);
-               }
+        while (message_repeat.equals("y")){
 
-               // compute PI with the result of each workers
-               for(int i = 0 ; i < numWorkers ; i++) {
-                   total += Integer.parseInt(tab_total_workers[i]);
-               }
-               pi = 4.0 * total / totalCount / numWorkers;
+            startTime = System.currentTimeMillis();
+            // initialize workers
+            for(int i = 0 ; i < numWorkers ; i++) {
+                writer[i].println(message_to_send);          // send a message to each worker
+            }
 
-               stopTime = System.currentTimeMillis();
+            //listen to workers's message
+            for(int i = 0 ; i < numWorkers ; i++) {
+                tab_total_workers[i] = reader[i].readLine();      // read message from server
+                System.out.println("Client sent: " + tab_total_workers[i]);
+            }
 
-               System.out.println("\nPi : " + pi );
-               System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
+            // compute PI with the result of each workers
+            for(int i = 0 ; i < numWorkers ; i++) {
+                total += Integer.parseInt(tab_total_workers[i]);
+            }
+            pi = 4.0 * total / totalCount / numWorkers;
 
-               System.out.println("Ntot: " + totalCount*numWorkers);
-               System.out.println("Available processors: " + numWorkers);
-               System.out.println("Time Duration (ms): " + (stopTime - startTime) + "\n");
+            stopTime = System.currentTimeMillis();
 
-               System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+            System.out.println("\nPi : " + pi );
+            System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
 
-               System.out.println("\n Repeat computation (y/N): ");
-               try{
-                   message_repeat = bufferRead.readLine();
-                   System.out.println(message_repeat);
-               }
-               catch(IOException ioE){
-                   ioE.printStackTrace();
-               }
-           }
+            System.out.println("Ntot: " + totalCount*numWorkers);
+            System.out.println("Available processors: " + numWorkers);
+            System.out.println("Time Duration (ms): " + (stopTime - startTime) + "\n");
 
-           for(int i = 0 ; i < numWorkers ; i++) {
-               System.out.println("END");     // Send ending message
-               writer[i].println("END") ;
-               reader[i].close();
-               writer[i].close();
-               sockets[i].close();
-           }
-   }
+            System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+
+            System.out.println("\n Repeat computation (y/N): ");
+            try{
+                message_repeat = bufferRead.readLine();
+                System.out.println(message_repeat);
+            }
+            catch(IOException ioE){
+                ioE.printStackTrace();
+            }
+        }
+
+        for(int i = 0 ; i < numWorkers ; i++) {
+            System.out.println("END");     // Send ending message
+            writer[i].println("END") ;
+            reader[i].close();
+            writer[i].close();
+            sockets[i].close();
+        }
+    }
 }
--- distributedMC_step1/WorkerSocket.java
+++ distributedMC_step1/WorkerSocket.java
@@ -1,18 +1,19 @@
 import java.io.*;
 import java.net.*;
 /**
- * Worker is a server. It computes PI by Monte Carlo method and sends 
+ * Worker is a server. It computes PI by Monte Carlo method and sends
  * the result to Master.
  */
 public class WorkerSocket {
     static int port = 25545; //default port
     private static boolean isRunning = true;
-    
+
     /**
-     * compute PI locally by MC and sends the number of points 
-     * inside the disk to Master. 
+     * compute PI locally by MC and sends the number of points
+     * inside the disk to Master.
      */
     public static void main(String[] args) throws Exception {
+
 
         if (!("".equals(args[0]))) port=Integer.parseInt(args[0]);
         System.out.println(port);
@@ -23,6 +24,7 @@ public class WorkerSocket {
         // BufferedReader bRead for reading message from Master
         BufferedReader bRead = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
+
         // PrintWriter pWrite for writing message to Master
         PrintWriter pWrite = new PrintWriter(new BufferedWriter(new OutputStreamWriter(soc.getOutputStream())), true);
         String str;
@@ -30,9 +32,30 @@ public class WorkerSocket {
             str = bRead.readLine();          // read message from Master
             if (!(str.equals("END"))){
                 System.out.println("Server receives totalCount = " +  str);
-                // compute
-                System.out.println("TODO : compute Monte Carlo and send total");
-                pWrite.println(str);
+
+
+                // compute Monte Carlo
+                int numIterations = Integer.parseInt(str);
+                long circleCount = 0;
+
+
+                java.util.Random prng = new java.util.Random();
+
+
+                for (int j = 0; j < numIterations; j++) {
+                    double x = prng.nextDouble();
+                    double y = prng.nextDouble();
+                    if ((x * x + y * y) < 1) {
+                        ++circleCount;
+                    }
+                }
+
+
+                System.out.println("Server sends circleCount = " + circleCount);
+
+
+                // send number of points in quarter of disk
+                pWrite.println(circleCount);
             }else{
                 isRunning=false;
             }
@@ -42,3 +65,4 @@ public class WorkerSocket {
         soc.close();
     }
 }
+
